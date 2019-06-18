facbb3e4412270b4024b7d7932d90d9bbf0e84f3
Stefanos <stefanosu422@gmail.com>
2019-06-18 13:41:09 -0400

    updated usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,39 @@
 class UsersController < ApplicationController
     
+def index 
+    @users = User.all 
+end
+
+def new 
+    @user = User.new 
+end
+
+def create 
+    user = User.create(user_params)
+    redirect_to user
+end
+
+def show 
+    @user = User.find(params[:id]) 
+    
+end
+
+def edit 
+    @user = User.find(params[:id]) 
+
+end
+
+def update 
+    user = User.find(params[:id]) 
+    user.destroy 
+    redirect_to 
+end
+
+private 
+
+def user_params
+    params.require(:user).permit(:username, :password, :first_name, :last_name)
+end 
+
 
 end
